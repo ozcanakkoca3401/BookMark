0ab52d03365be4fe8cf84b766b07aa585d3d242e
Özcan AKKOCA <ozcan_akkoca@hotmail.com>
2018-02-16 11:56:01 +0300

    Pod init

---
+++ Pods/Target Support Files/Pods-bookmarkmobile bookmarkmobile.xcodeproj bookmarkmobile.xcworkspace bookmarkmobileTests bookmarkmobileUITests Podfile Podfile.lock bookmarkmobile/Pods-bookmarkmobile bookmarkmobile.xcodeproj bookmarkmobile.xcworkspace bookmarkmobileTests bookmarkmobileUITests Podfile Podfile.lock bookmarkmobile-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_bookmarkmobile_bookmarkmobile_xcodeproj_bookmarkmobile_xcworkspace_bookmarkmobileTests_bookmarkmobileUITests_Podfile_Podfile_lock_bookmarkmobileVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_bookmarkmobile_bookmarkmobile_xcodeproj_bookmarkmobile_xcworkspace_bookmarkmobileTests_bookmarkmobileUITests_Podfile_Podfile_lock_bookmarkmobileVersionString[];
+
