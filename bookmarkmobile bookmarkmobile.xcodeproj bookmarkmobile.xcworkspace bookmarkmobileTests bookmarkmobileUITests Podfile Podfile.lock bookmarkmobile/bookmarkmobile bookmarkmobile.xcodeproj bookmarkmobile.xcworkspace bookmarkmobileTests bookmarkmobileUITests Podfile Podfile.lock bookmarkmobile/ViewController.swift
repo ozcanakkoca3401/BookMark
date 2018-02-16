5987ea435d09e75f5add78c900f800e37484648a
Özcan AKKOCA <ozcan_akkoca@hotmail.com>
2018-02-16 11:52:18 +0300

    Project commit

---
+++ bookmarkmobile bookmarkmobile.xcodeproj bookmarkmobile.xcworkspace bookmarkmobileTests bookmarkmobileUITests Podfile Podfile.lock bookmarkmobile/bookmarkmobile bookmarkmobile.xcodeproj bookmarkmobile.xcworkspace bookmarkmobileTests bookmarkmobileUITests Podfile Podfile.lock bookmarkmobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bookmarkmobile bookmarkmobile.xcodeproj bookmarkmobile.xcworkspace bookmarkmobileTests bookmarkmobileUITests Podfile Podfile.lock bookmarkmobile
+//
+//  Created by Özcan AKKOCA on 16.02.2018.
+//  Copyright © 2018 Özcan AKKOCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ bookmarkmobile bookmarkmobile.xcodeproj bookmarkmobile.xcworkspace bookmarkmobileTests bookmarkmobileUITests Podfile Podfile.lock bookmarkmobile/bookmarkmobile bookmarkmobile.xcodeproj bookmarkmobile.xcworkspace bookmarkmobileTests bookmarkmobileUITests Podfile Podfile.lock bookmarkmobileTests/bookmarkmobile_bookmarkmobile_xcodeproj_bookmarkmobile_xcworkspace_bookmarkmobileTests_bookmarkmobileUITests_Podfile_Podfile_lock_bookmarkmobileTests.swift
@@ -0,0 +1,36 @@
+//
+//  bookmarkmobile_bookmarkmobile_xcodeproj_bookmarkmobile_xcworkspace_bookmarkmobileTests_bookmarkmobileUITests_Podfile_Podfile_lock_bookmarkmobileTests.swift
+//  bookmarkmobile bookmarkmobile.xcodeproj bookmarkmobile.xcworkspace bookmarkmobileTests bookmarkmobileUITests Podfile Podfile.lock bookmarkmobileTests
+//
+//  Created by Özcan AKKOCA on 16.02.2018.
+//  Copyright © 2018 Özcan AKKOCA. All rights reserved.
+//
+
+import XCTest
+@testable import bookmarkmobile_bookmarkmobile_xcodeproj_bookmarkmobile_xcworkspace_bookmarkmobileTests_bookmarkmobileUITests_Podfile_Podfile_lock_bookmarkmobile
+
+class bookmarkmobile_bookmarkmobile_xcodeproj_bookmarkmobile_xcworkspace_bookmarkmobileTests_bookmarkmobileUITests_Podfile_Podfile_lock_bookmarkmobileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
