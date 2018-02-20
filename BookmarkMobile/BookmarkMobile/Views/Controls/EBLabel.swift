25d23cd4fe0f2ec4e6ef2a40285be58092c39e52
Umut Karagöz <umut.karagoz@etiya.com>
2018-02-20 17:24:51 +0300

    label
    label dosyası eklendi

---
+++ BookmarkMobile/BookmarkMobile/Views/Controls/EBLabel.swift
@@ -0,0 +1,21 @@
+//
+//  EBLabel.swift
+//  BookmarkMobile
+//
+//  Created by Umut on 20.02.2018.
+//  Copyright © 2018 Nookmark. All rights reserved.
+//
+
+import UIKit
+
+class EBLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
