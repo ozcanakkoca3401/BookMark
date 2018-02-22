3d4836576a4c51dd4e924802db38199cec4b0053
Umut Karagöz <umut.karagoz@etiya.com>
2018-02-22 13:31:28 +0300

    amend
    amend

---
+++ BookmarkMobile/BookmarkMobile/Views/Controls/CHorizontalSlider.swift
@@ -0,0 +1,46 @@
+//
+//  CHorizontalSlider.swift
+//  BookmarkMobile
+//
+//  Created by Umut on 22.02.2018.
+//  Copyright © 2018 Nookmark. All rights reserved.
+//
+
+import UIKit
+
+class CHorizontalSlider: HorizontalSlider {
+
+    @IBInspectable var isThumbInteractionEnabled: Bool = true {
+        didSet{
+            isUserInteractionEnabled = isThumbInteractionEnabled
+        }
+    }
+
+    @IBInspectable var thumbColor: UIColor = Styling.colorForCode(.themeGray) {
+        didSet{
+            thumbTintColor = thumbColor
+        }
+    }
+
+    public override init() {
+        super.init(frame: .zero)
+        self.initialize()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initialize()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initialize()
+    }
+    
+    override func initialize() {
+        super.initialize()
+        thumbTintColor = thumbColor
+        isUserInteractionEnabled = isThumbInteractionEnabled
+    }
+    
+}
