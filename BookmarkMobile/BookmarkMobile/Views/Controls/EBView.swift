6aa9a4cbdca29115a37cb9e400e5070281e2427a
Umut Karagöz <umut.karagoz@etiya.com>
2018-02-22 11:18:56 +0300

    EBView
    EBView oluşturuldu

---
+++ BookmarkMobile/BookmarkMobile/Views/Controls/EBView.swift
@@ -0,0 +1,32 @@
+//
+//  EBView.swift
+//  BookmarkMobile
+//
+//  Created by Umut on 22.02.2018.
+//  Copyright © 2018 Nookmark. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class EBView: UIView {
+
+    
+    public init() {
+        super.init(frame: .zero)
+        self.initialize()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initialize()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initialize()
+    }
+    
+    func initialize(){
+        
+    }
+}
