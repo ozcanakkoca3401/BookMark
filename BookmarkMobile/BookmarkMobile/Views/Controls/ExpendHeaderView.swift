ab71ef9bd4cb04991ad1fb203beb6e7ff3dc31a2
bilge.yasa <bilgeyasa2626@gmail.com>
2018-03-01 10:16:03 +0300

    add expendHeaderView

--- BookmarkMobile/BookmarkMobile/Views/Controls/ExpendHeaderView.swift
+++ BookmarkMobile/BookmarkMobile/Views/Controls/ExpendHeaderView.swift
@@ -29,10 +29,17 @@ import SnapKit
         }
     }
     
+//    @IBInspectable var upToDate: UIButton? {
+//        didSet {
+//            
+//        }
+//    }
+    
     var phoneImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(named: "phone")
+        imageView.image = UIImage(named: "blackPhone")
         imageView.contentMode = .scaleAspectFit
+        
         return imageView
     }()
     
@@ -41,7 +48,7 @@ import SnapKit
         label.EBtext = "514-428-0578"
         label.EBcolor = Styling.colorForCode(.themeGray)
         label.textAlignment = .center
-        label.font = Styling.font(weight: .bold, size: 15)
+        label.font = Styling.font(weight: .bold, size: 13)
         
         return label
     }()
@@ -54,6 +61,15 @@ import SnapKit
         label.font = Styling.font(weight: .bold, size: 15)
         
         return label
+    }()
+    
+    var upToDate: UIButton = {
+        let button = UIButton()
+        button.setTitle("Up To Date", for: .normal)
+        button.setTitleColor(Styling.colorForCode(.themeGray), for: .normal)
+        button.titleLabel?.font = Styling.font(weight: .regular, size: 13)
+        
+        return button
     }()
     
     // code initialize
@@ -83,24 +99,32 @@ import SnapKit
         self.addSubview(phoneImageView)
         self.addSubview(leftLabel)
         self.addSubview(gbLabel)
-        
-        phoneImageView.snp.makeConstraints { (make) in
-            make.width.equalTo(30)
-            make.height.equalTo(30)
-            make.left.equalTo(leftLabel.snp.right).offset(70)
-            make.centerX.equalTo(self)
-        }
+        self.addSubview(upToDate)
         
        leftLabel.snp.makeConstraints { (make) in
-            make.left.equalTo(self).offset(10)
+            make.left.equalTo(self).offset(15)
             make.bottom.equalTo(self).dividedBy(2).offset(5)
             make.height.equalTo(30)
         }
         
         gbLabel.snp.makeConstraints { (make) in
             make.top.equalTo(leftLabel.snp.bottom).offset(-10)
-            make.left.equalTo(self).offset(10)
+            make.left.equalTo(self).offset(15)
             make.height.equalTo(30)
+        }
+        
+        phoneImageView.snp.makeConstraints { (make) in
+            make.top.equalTo(self).offset(0)
+            make.bottom.equalTo(self).offset(0)
+            make.centerX.equalTo(self)
+        }
+        
+        upToDate.snp.makeConstraints { (make) in
+            make.top.equalTo(self).offset(0)
+            make.bottom.equalTo(self).offset(0)
+            make.right.equalTo(self).offset(-15)
+            //make.right.equalTo(phoneImageView.snp.left).offset(-15)
+            make.centerY.equalTo(self)
         }
         
     }
