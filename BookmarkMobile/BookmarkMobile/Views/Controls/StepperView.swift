3d4442c4dfafdf35b7282c77435bcf5841659695
bilge.yasa <bilgeyasa2626@gmail.com>
2018-03-02 13:57:23 +0300

    add StepperView file

---
+++ BookmarkMobile/BookmarkMobile/Views/Controls/StepperView.swift
@@ -0,0 +1,116 @@
+//
+//  StepperView.swift
+//  BookmarkMobile
+//
+//  Created by bilge.yasa on 2.03.2018.
+//  Copyright © 2018 Nookmark. All rights reserved.
+//
+
+import Foundation
+import SnapKit
+
+@IBDesignable public class StepperView: RoundView {
+    
+//    @IBInspectable var icon: String = "" {
+//        didSet {
+//            iconLabel.text = icon
+//        }
+//    }
+//    
+//    @IBInspectable var price: String = "" {
+//        didSet {
+//            priceLabel.text = price
+//        }
+//    }
+//    
+//    @IBInspectable var product: String = "" {
+//        didSet {
+//            productDescriptionLabel.text = product
+//        }
+//    }
+//    
+//    var iconLabel: EBLabel = {
+//        let label = EBLabel()
+//        label.EBtext = "$"
+//        label.EBcolor = Styling.colorForCode(.themeBlue)
+//        label.textAlignment = .center
+//        label.font = Styling.font(weight: .bold, size: 20)
+//        
+//        return label
+//    }()
+//    
+//    var priceLabel: EBLabel = {
+//        let label = EBLabel()
+//        label.EBtext = "5"
+//        label.EBcolor = Styling.colorForCode(.themeBlue)
+//        label.textAlignment = .center
+//        label.font = Styling.font(weight: .bold, size: 20)
+//        
+//        return label
+//    }()
+//    
+//    var productDescriptionLabel: EBLabel = {
+//        let label = EBLabel()
+//        label.EBtext = "500 MB Shareable Data"
+//        label.textAlignment = .left
+//        label.font = UIFont.systemFont(ofSize: 15)
+//        
+//        return label
+//    }()
+//    
+//    // code initialize
+//    public override init() {
+//        super.init()
+//        
+//        self.initialize()
+//    }
+//    
+//    // code initialize
+//    public override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        
+//        self.initialize()
+//    }
+//    
+//    // storyboard initialize
+//    required public init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        
+//        self.initialize()
+//    }
+//    
+//    override func initialize() {
+//        
+//        super.initialize()
+//        self.addSubview(iconLabel)
+//        self.addSubview(priceLabel)
+//        self.addSubview(productDescriptionLabel)
+//        
+//        iconLabel.snp.makeConstraints { (make) in
+//            make.width.equalTo(30)
+//            make.height.equalTo(30)
+//            make.left.equalTo(self).offset(15)
+//            make.centerY.equalTo(self)
+//        }
+//        
+//        priceLabel.snp.makeConstraints { (make) in
+//            make.width.equalTo(30)
+//            make.height.equalTo(30)
+//            make.left.equalTo(iconLabel).offset(15)
+//            make.centerY.equalTo(self)
+//        }
+//        
+//        productDescriptionLabel.snp.makeConstraints { (make) in
+//            make.height.equalTo(20)
+//            make.left.equalTo(priceLabel).offset(30)
+//            make.right.equalTo(self).offset(-15)
+//            make.centerY.equalTo(self)
+//        }
+//    }
+//    
+//    override public func layoutSubviews() {
+//        super.layoutSubviews()
+//    }
+    
+}
+
