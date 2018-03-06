1c8ce371585d45fc8efa8cfff65a37566c9a7314
Umut Karagöz <umut.karagoz@etiya.com>
2018-03-06 18:51:26 +0300

    loyaltyView
    loyaltyView

---
+++ BookmarkMobile/BookmarkMobile/Views/Components/EBTopLoyaltyView.swift
@@ -0,0 +1,15 @@
+//
+//  EBTopLoyaltyView.swift
+//  BookmarkMobile
+//
+//  Created by Umut on 6.03.2018.
+//  Copyright © 2018 Nookmark. All rights reserved.
+//
+
+import UIKit
+
+class EBTopLoyaltyView: RoundView {
+
+   
+
+}
