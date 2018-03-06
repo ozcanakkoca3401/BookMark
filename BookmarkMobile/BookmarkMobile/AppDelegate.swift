f6221ca0f0911b19a02f05075f9b8be3d7fbfb4b
bilge.yasa <bilgeyasa2626@gmail.com>
2018-03-06 16:54:26 +0300

    add informationCell

--- BookmarkMobile/BookmarkMobile/AppDelegate.swift
+++ BookmarkMobile/BookmarkMobile/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.makeKeyAndVisible()
-        self.window?.rootViewController = UINavigationController(rootViewController: EnesViewController())
+        self.window?.rootViewController = UINavigationController(rootViewController: BilgeViewController())
         return true
     }
 
---
+++ BookmarkMobile/BookmarkMobile/Views/Components/InformationCellView.swift
@@ -0,0 +1,85 @@
+//
+//  InformationCellView.swift
+//  BookmarkMobile
+//
+//  Created by bilge.yasa on 6.03.2018.
+//  Copyright © 2018 Nookmark. All rights reserved.
+//
+
+import Foundation
+import SnapKit
+
+@IBDesignable public class InformationCellView: RoundView {
+    
+    var nameLabel: EBLabel = {
+        let label = EBLabel()
+        label.EBtext = "name"
+        label.EBcolor = Styling.colorForCode(.themeGray)
+        label.font = Styling.font(weight: .regular, size: 15)
+        return label
+    }()
+    
+    var secondNameLabel: EBLabel = {
+        let label = EBLabel()
+        label.EBtext = "John Doe"
+        //label.textAlignment = .right
+        label.EBcolor = Styling.colorForCode(.black)
+        label.font = Styling.font(weight: .regular, size: 15)
+        return label
+    }()
+    
+    var editButton: RoundButton = {
+        let button = RoundButton()
+        button.setImage(UIImage(named: "edit"), for: .normal)
+        
+        return button
+    }()
+    
+    public override init() {
+        super.init(frame: .zero)
+        self.initialize()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initialize()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initialize()
+    }
+    
+    override func initialize() {
+     
+        self.borderColor = Styling.colorForCode(.themeGray)
+        self.borderWidth = 2
+        
+        self.addSubview(nameLabel)
+        self.addSubview(secondNameLabel)
+        self.addSubview(editButton)
+        
+        
+        nameLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(self).offset(0)
+            make.left.equalTo(self).offset(10)
+            make.height.equalTo(self).dividedBy(2)
+            make.width.equalTo(50)
+        }
+        
+        secondNameLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(nameLabel.snp.bottom).offset(0)
+            make.left.equalTo(self).offset(10)
+            make.height.equalTo(self).dividedBy(2)
+            make.width.equalTo(130)
+        }
+        
+        editButton.snp.makeConstraints { (make) in
+            make.height.equalTo(40)
+            make.width.equalTo(40)
+            make.top.equalTo(10)
+            make.right.equalTo(-10)
+        }
+  
+    }
+}
