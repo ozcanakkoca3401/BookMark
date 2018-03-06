958536f1d3ad9d9de8b74c86f5a1b4359154aa68
bilge.yasa <bilgeyasa2626@gmail.com>
2018-03-06 17:48:14 +0300

    add InformationTableViewController

--- BookmarkMobile/BookmarkMobile/Views/Cells/InformationTableViewCell.swift
+++ BookmarkMobile/BookmarkMobile/Views/Cells/InformationTableViewCell.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class InformationTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let cellView = InformationCellView()
+        
+        cellView.frame = self.frame
+        print("Frame", self.frame)
+        contentView.addSubview(cellView)
+   
     }
+        
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
 }
