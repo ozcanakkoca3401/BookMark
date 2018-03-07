5b9ab0be42c33d0577e3333b16a69f2d3d63a44f
bilge.yasa <bilgeyasa2626@gmail.com>
2018-03-07 15:18:42 +0300

    amend

--- BookmarkMobile/BookmarkMobile/Views/Cells/InformationTableViewCell.swift
+++ BookmarkMobile/BookmarkMobile/Views/Cells/InformationTableViewCell.swift
@@ -21,16 +21,16 @@ class InformationTableViewCell: UITableViewCell {
    
     }
         
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
